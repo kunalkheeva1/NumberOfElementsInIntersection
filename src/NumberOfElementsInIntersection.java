b278ed34f508140a72d4cd9ecaa48e5eeda8b5bd
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 02:49:03 -0700

    created a method to check the number of the elements responsible for the intersection of the array from two arrays

--- src/NumberOfElementsInIntersection.java
+++ src/NumberOfElementsInIntersection.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
 public class NumberOfElementsInIntersection {
-    
+    public static int numberOfElementsInIntersection(int arr1[], int arr2[], int n , int m){
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            map.put(arr1[i],true );
+        }
+        int count = 0;
+        for(int i=0; i<m; i++){
+            if(map.containsKey(arr2[i])){
+                count++;
+                map.remove(arr2[i]);
+            }
+        }return count;
+    }
 
     public static void main(String[] args) {
 
